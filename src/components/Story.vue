2132f1271c2c865298fa8701fd488e6d8c006d9b
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-14 22:28:16 +0300

    Update story layout

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="story">
-    <Stories :stories="[[1, 2, 3]]" />
+  <div id="container">
+    <Stories id="story" :stories="stories" :slideDuration="4000" />
   </div>
 </template>
 
@@ -11,9 +11,26 @@ export default {
   name: "Story",
   components: {
     Stories
+  },
+  data: () => {
+    return {
+      stories: [[1, 2, 3]]
+    };
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+
+#container {
+  margin: 0 auto;
+  height: 100vh;
+  width: calc(100vh * 0.5625); // for some reason aspect ratio can't be used as a variable
+}
+
+#story {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
 </style>
