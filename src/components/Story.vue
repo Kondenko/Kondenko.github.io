1aae4deeec98277c18c3cfe03f72a59100f7b72e
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-14 19:17:54 +0300

    Add a route to the Story page

---
+++ src/components/Story.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="story">
+      Story
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Story"
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './components/App.vue'
-import VModal from "vue-js-modal";
+
+import VModal from "vue-js-modal"
 import VueAnalytics from 'vue-analytics'
 import VueScrollTo from 'vue-scrollto'
-import VueCarousel from "@chenfengyuan/vue-carousel";
+import VueCarousel from "@chenfengyuan/vue-carousel"
 
-Vue.use(VueRouter)
-Vue.use(VueCarousel);
-Vue.use(VModal, { dynamic: true });
+import App from './components/App.vue'
+import Story from './components/Story.vue'
+
+Vue.use(VueCarousel)
+Vue.use(VModal, { dynamic: true })
 Vue.use(VueAnalytics, {
   id: 'UA-120562930-1',
   debug: {
     sendHitTask: process.env.NODE_ENV === 'production'
   }
-});
+})
 Vue.use(VueScrollTo)
+
+const routes = {
+ '/': App,
+ '/story': Story
+}
 
 new Vue({
   el: "#app",
-  render: h => h(App)
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute] || App
+    }
+  },
+  render(h) { return h(this.ViewComponent) }
 }).$mount('#app')
