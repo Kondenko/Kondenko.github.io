3cb5087bed80e1f2f52e20c003bb80239b843afe
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-17 17:00:01 +0300

    Show stories in fullscreen mode

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="container">
-    <Stories id="story" :stories="stories" :slideDuration="4000" />
+    <Stories v-if="isFullscreen" id="story" :stories="stories" :slideDuration="4000" />
+    <img :src="avatar" @click="showStory" v-if="!isFullscreen" id="image_story" />
   </div>
 </template>
 
 <script>
 import Stories from "stories/src/Stories.vue";
+import TextButton from "ui/TextButton.vue";
+const utils = require("src/utils.js");
 
 export default {
   name: "Story",
   components: {
-    Stories
+    Stories,
+    TextButton
   },
   data: () => {
     return {
+      isFullscreen: false,
+      avatar: require("assets/img_avatar.png"),
       stories: [
         [
           require("assets/story/1.gif"),
@@ -22,18 +28,28 @@ export default {
           require("assets/story/4.gif"),
           require("assets/story/5.gif"),
           require("assets/story/6.gif"),
-          require("assets/story/7.gif"),
+          require("assets/story/7.gif")
         ]
       ]
     };
+  },
+  mounted: function() {},
+  methods: {
+    showStory: function() {
+      utils.toggleFullscreen();
+      this.isFullscreen = true;
+    }
   }
 };
 </script>
 
 <style lang="scss">
+@import "~res/colors";
+
 #container {
   margin: 0 auto;
   height: 100vh;
+  position: relative;
   width: calc(
     100vh * 0.5625
   ); // for some reason aspect ratio can't be used as a variable
@@ -44,9 +60,29 @@ export default {
 }
 
 #story {
-  position: relative;
   height: 100%;
   width: 100%;
+  position: absolute;
+  left: 0;
+}
+
+$size: 72px;
+#image_story {
+  /* Position */
+  width: $size;
+  height: $size;
+  position: absolute;
+  left: 50%;
+  margin-left: -($size / 2);
+  top: 50%;
+  margin-top: -($size / 2);
+  border-radius: 3px;
+  /* Border */
+  border-style: solid;
+  border-radius: 100%;
+  border-width: 3px;
+  padding: 2px;
+  border-color: $accentColor;
 }
 
 .timeline > .slice {
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,24 @@
 module.exports = {
-    isElementActive: function(el) {
+    isElementActive: function (el) {
         let threshhold = 100;
         let rect = el.getBoundingClientRect();
         return rect.top <= threshhold;
     },
-    emitIfIsVisible: function(component, id) {
+    emitIfIsVisible: function (component, id) {
         let isVisible = this.isElementActive(document.getElementById(id));
         if (isVisible) component.$emit("isVisible", id);
+    },
+    toggleFullscreen: function () {
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+            requestFullScreen.call(docEl);
+        } else {
+            cancelFullScreen.call(doc);
+        }
     }
 }
